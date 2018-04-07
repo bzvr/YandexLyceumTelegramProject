435adde3ddae23f59861a7e2a9d258c97fd779f0
Andrew <saintruler@gmail.com>
2018-04-07 22:04:20 +0400

    Removed converters as useless. Slightly refactored code.
    Signed-off-by: Andrew <saintruler@gmail.com>

--- speech_analyze.py
+++ speech_analyze.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 def speech_analyze(apikey, data):
     uuid = str(uuid4()).replace('-', '')
     url = 'https://asr.yandex.net/asr_xml?key=' + apikey + '&uuid=' + uuid + '&topic=queries&lang=ru-RU'
-    headers = {"Content-Type": 'audio/x-wav'}
+    headers = {"Content-Type": 'audio/ogg;codecs=opus'}
     tmp = requests.post(url, headers=headers, data=data)
 
     return tmp
